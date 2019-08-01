1d502e9fe808465759b6c0b8a579b6c233aa990a
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2019-08-01 17:02:17 +1000

    clean-up repository tree for debugging

--- snpEff/scripts/countColumns.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import sys
-
-for line in sys.stdin:
-	line = line.rstrip()
-	print "{}\t{}".format( len( line.split('\t') ), line)
--- snpEff/scripts/gsa/bayesFactor_correction_scoreCount.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-in="$1"
-out="$2"
-
-Rscript `dirname $0`/bayesFactor_correction_scoreCount.r "$in" "$out" -1
